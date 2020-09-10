5723a8e3ae0b9bfb94bdb168406a3f75c75534a2
MIkhaaa99 <70886962+MIkhaaa99@users.noreply.github.com>
2020-09-10 12:10:52 +0300

    commit1

---
+++ !Spirit Vessel!/Assets/Scripts/InstantiationExample.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiationExample : MonoBehaviour
+{
+
+    public GameObject myPrefab;
+    public float SpawnTime;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnBlock", 0, SpawnTime);
+    }
+
+    void SpawnBlock()
+    {
+        GameObject block = Instantiate(myPrefab, transform.position, transform.rotation) as GameObject;
+    }
+}
---
+++ !Spirit Vessel!/Assets/Scripts/MoveMobs.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMobs : MonoBehaviour
+{
+    public GameObject finish;
+
+    void Start()
+    {
+        finish = GameObject.Find("Finish");
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, finish.transform.position, Time.deltaTime);
+    }
+}
